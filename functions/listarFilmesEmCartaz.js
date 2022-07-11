c0714007aff99f9a440b59d8218e219430a2770c
matheusclima <matheuslima1607@gmail.com>
2022-07-11 10:09:10 -0300

    Melhoria no código da função listarFilmesEmCartaz

--- functions/listarFilmesEmCartaz.js
+++ functions/listarFilmesEmCartaz.js
@@ -1,10 +1,6 @@
+const listarFilmes = require("./listarTodosOsFilmes")
+
 module.exports = (catalogo) => {
     let filmesEmCartaz = catalogo.filter(filme => filme.emCartaz)
-    console.table(filmesEmCartaz.map(filme => {
-        return {
-            codigo: filme.codigo,
-            titulo: filme.titulo,
-            duracao: filme.duracao
-        }
-    }))
+    listarFilmes(filmesEmCartaz)
 }
